107c4778602e539384bcd8acc66faf689f57273c
CoreyGaunt <65140255+CoreyGaunt@users.noreply.github.com>
2024-04-05 15:24:01 -0500

    Feat: add new command

--- src/tools.ts
+++ src/tools.ts
@@ -17,6 +17,46 @@ const completionSpec: Fig.Spec = {
 		{
 			name: "branch-new",
 			description: "Checks out main, pulls latest changes, and creates a new branch. Can be used for Linear Ticket Connections",
+		},
+		{
+			name: "pr-create",
+			description: "Leverages the GH CLI to create a new PR with the current branch. Requires a PULL_REQUEST_TEMPLATE environment variable to be set",
+		},
+		{
+			name: "run",
+			description: "Searches through SQL files in the models/ directory and runs the specified file. Can include production and upstream/downstream flags",
+			options: [
+				{
+					name: ["-p", "--production"],
+					description: "Run the query in production",
+				},
+				{
+					name: ["-u", "--upstream"],
+					description: "Run the query in upstream",
+					args: {
+						name: "level",
+						description: "The level of upstreams to run from the specified model",
+						default: "",
+						isOptional: true,
+						suggestions: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+					}
+				},
+				{
+					name: ["-d", "--downstream"],
+					description: "Run the query in downstream",
+					args: {
+						name: "level",
+						description: "The level of downstreams to run from the specified model",
+						default: "",
+						isOptional: true,
+						suggestions: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+					}
+				},
+				{
+					name: ["-a", "--waterfall"],
+					description: "Run the specified model, its downstreams, and its downstreams' upstreams",
+				}
+			]
 		}
 	],
 };
